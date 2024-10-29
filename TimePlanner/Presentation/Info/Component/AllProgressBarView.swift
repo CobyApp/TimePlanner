53e6c64c81b311c1bb599a4be18e8028309d5970
Coby <coby5502@gmail.com>
2024-10-29 22:54:39 +0900

    [FIX] 빈막대 오류 수정

--- TimePlanner/Presentation/Info/Component/AllProgressBarView.swift
+++ TimePlanner/Presentation/Info/Component/AllProgressBarView.swift
@@ -30,12 +30,33 @@ final class AllProgressBarView: UIView {
     
     // MARK: - Configure
     func configure(with data: [ProgressBarModel]) {
-        guard !data.isEmpty else { return }
-        
         self.setupBars() // 기존 막대 뷰 초기화
 
-        let totalSum = data.reduce(0) { $0 + $1.totalCount } // 모든 totalCount의 합
+        guard !data.isEmpty else {
+            // 데이터가 비어있을 경우, 전체를 회색으로 채움
+            let barHeight: CGFloat = 20
+            let fillBar = UIView(frame: CGRect(x: 0, y: 0, width: SizeLiteral.fullWidth, height: barHeight))
+            fillBar.backgroundColor = .fillStrong // 회색으로 설정
+            self.addSubview(fillBar) // 회색 막대 추가
+            self.barViews.append(fillBar) // 배열에 저장
+            self.layoutIfNeeded()
+            return
+        }
         
+        let totalSum = data.reduce(0) { $0 + $1.totalCount } // 모든 totalCount의 합
+
+        // totalCount가 0인 경우 처리
+        guard totalSum > 0 else {
+            // totalCount가 0일 경우에도 회색으로 채움
+            let barHeight: CGFloat = 20
+            let fillBar = UIView(frame: CGRect(x: 0, y: 0, width: SizeLiteral.fullWidth, height: barHeight))
+            fillBar.backgroundColor = .fillStrong // 회색으로 설정
+            self.addSubview(fillBar) // 회색 막대 추가
+            self.barViews.append(fillBar) // 배열에 저장
+            self.layoutIfNeeded()
+            return
+        }
+
         let totalWidth = SizeLiteral.fullWidth
         let barHeight: CGFloat = 20
         var xOffset: CGFloat = 0 // x 위치 조정
@@ -44,12 +65,15 @@ final class AllProgressBarView: UIView {
             let proportion = Double(item.completionCount) / Double(totalSum) // 비율 계산
             let barWidth = CGFloat(proportion) * totalWidth // 막대 너비
             
-            let barView = UIView(frame: CGRect(x: xOffset, y: 0, width: barWidth, height: barHeight))
-            barView.backgroundColor = item.completedColor // 해당 색상 설정
-            self.addSubview(barView) // 막대 추가
-            self.barViews.append(barView) // 배열에 저장
-            
-            xOffset += barWidth // x 위치 업데이트
+            // barWidth가 0보다 클 때만 막대 추가
+            if barWidth > 0 {
+                let barView = UIView(frame: CGRect(x: xOffset, y: 0, width: barWidth, height: barHeight))
+                barView.backgroundColor = item.completedColor // 해당 색상 설정
+                self.addSubview(barView) // 막대 추가
+                self.barViews.append(barView) // 배열에 저장
+                
+                xOffset += barWidth // x 위치 업데이트
+            }
         }
         
         // 남은 부분 회색으로 설정
